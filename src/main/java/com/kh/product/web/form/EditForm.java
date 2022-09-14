9ed1f6785c9ed52b9442049dd2323ff8fdc83c38
leekione <byesg1127@naver.com>
2022-09-14 17:35:09 +0900

    20220914

---
+++ src/main/java/com/kh/product/web/form/EditForm.java
@@ -0,0 +1,23 @@
+package com.kh.product.web.form;
+
+import lombok.Data;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+
+@Data
+public class EditForm {
+    private Long pid;
+    @NotBlank
+    private String pname;
+    @NotNull
+    @Positive
+    @Max(9999999999L)
+    private Long count;
+    @NotNull
+    @Positive
+    @Max(9999999999L)
+    private Long price;
+}
